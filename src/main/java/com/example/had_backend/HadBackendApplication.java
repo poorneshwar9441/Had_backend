92b06fad77a72c35c016ced09820728f97b7a53d
poorneshwar9441 <poorneshchaganti@gmail.com>
2024-02-20 21:35:25 +0530

    latest

---
+++ src/main/java/com/example/had_backend/HadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.had_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/had_backend/HadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.had_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
