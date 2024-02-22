fdc9c7c3f245ab89d9da3fd1d05856d59a14970c
poorneshwar9441 <poorneshchaganti@gmail.com>
2024-02-22 14:39:21 +0530

    jwt authentication

---
+++ src/main/java/com/example/had_backend/repository/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package com.example.had_backend.repository;
+
+
+import com.example.had_backend.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
