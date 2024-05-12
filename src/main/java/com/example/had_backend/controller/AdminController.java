2ef1ebb1058883a3124704cc935019ac8942b36a
qdino <sriram.munagala@iiitb.ac.in>
2024-05-12 23:54:56 +0530

    Minor fixes

--- src/main/java/com/example/had_backend/controller/AdminController.java
+++ src/main/java/com/example/had_backend/controller/AdminController.java
@@ -1,32 +1,24 @@
 package com.example.had_backend.controller;
 
-import com.example.had_backend.entity.Admin;
 import com.example.had_backend.entity.Doctor;
 import com.example.had_backend.entity.UserInfo;
-import com.example.had_backend.service.AdminService;
 import com.example.had_backend.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AdminController {
     @Autowired
     private UserInfoService userInfoService;
-
-    @Autowired
-    private AdminService adminService;
-
-
     @PostMapping("/createAdmin")
     public ResponseEntity<?> createAdmin(@RequestBody UserInfo userInfo) {
-        System.out.println("logs");
         try {
-
-            userInfo.setRoles("Admin");
+            userInfo.setRoles("admin");
             UserInfo createdUserInfo = userInfoService.addUser(userInfo);
-            Admin a = adminService.createAdmin(createdUserInfo);
-            return ResponseEntity.ok(a);
+            return ResponseEntity.ok(createdUserInfo);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body("Username already exists!");
         }
